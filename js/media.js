6217efb398cb1433bcfc32db045ae0be0beba5da
SgtSkippy <dylanpadua@yahoo.com>
2025-01-25 15:47:16 -0800

    Added features for media carousel.

--- js/media.js
+++ js/media.js
@@ -1,6 +1,7 @@
-function displayMedia() {
-    const mediaBtn_open = document.querySelectorAll(".media-link");
-    const mediaBtn_close = document.querySelectorAll(".media-close");
+// Handles media window popup for all projects
+function displayMediaWindow() {
+    const mediaBtn_open = document.querySelectorAll(".media-link");     // Grabs all media buttons
+    const mediaBtn_close = document.querySelectorAll(".media-close");   // Grabs all close buttons
     const mediaOverlay = document.getElementById("mediaOverlay");
 
     // Media button handler
@@ -29,6 +30,24 @@ function displayMedia() {
             mediaOverlay.style.display = "none";
         });
     });
-}
+};
 
-document.addEventListener("DOMContentLoaded", displayMedia);
+// Loads images/videos for all projects
+function loadProjectMedia(project) {
+    const media = project.querySelector(".media-files");        // FIXME: project catches a type error
+    const mediaContent = project.querySelectorAll(".media-files img, .media-files video");
+    const previousButton = project.querySelector(".media-button.left");
+    const nextButton = project.querySelector(".media-button.right");
+    const dotsContainer = project.querySelector(".media-dots");
+
+    // TODO: Implement changing and updating media, generating dots for media count, and adding listeners to buttons
+};
+
+// Initializes all project media
+const projectMedia = document.querySelectorAll(".media-space");     // Grabs all media space from each project
+projectMedia.forEach(project => {
+    loadProjectMedia(project);
+});
+
+document.addEventListener("DOMContentLoaded", displayMediaWindow);
+document.addEventListener("DOMContentLoaded", loadProjectMedia);
